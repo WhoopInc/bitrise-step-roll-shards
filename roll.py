9c0e47226320299a9e893647eb554164a0a914d4
Swaraj Rao <swaraj.rao@whoop.com>
2022-02-11 12:35:40 -0500

    Using bracket notation everywhere

--- roll.py
+++ roll.py
@@ -7,14 +7,14 @@ def abort(build):
         'abort_with_success': 'true',
         'skip_notifications': 'true'
     }
-    abort_builds_url = 'https://{}/apps/{}/builds/{}/abort'.format(base_url, app_slug, build.slug)
+    abort_builds_url = 'https://{}/apps/{}/builds/{}/abort'.format(base_url, app_slug, build['slug'])
     request = requests.post(abort_builds_url, payload, request_headers)
     if request.status_code != 200:
-        print('Unable to abort workflow {}'.format(build.triggered_workflow))
+        print('Unable to abort workflow {}'.format(build['triggered_workflow']))
         print('Response: {}'.format(request.text))
     else:
-        print('Successfully aborted workflow {}'.format(build.triggered_workflow))
-        rolled_build_slugs_list.append(build.slug)
+        print('Successfully aborted workflow {}'.format(build['triggered_workflow']))
+        rolled_build_slugs_list.append(build['slug'])
     return rolled_build_slugs_list
 
 def main():
@@ -41,17 +41,16 @@ def main():
     }
     request = requests.get(running_builds_url, headers=request_headers)
     response = request.json()
-    print(response)
 
     running_builds = response['data']
     num_builds = response['paging']
-    print('Found {} running builds!'.format(num_builds.total_item_count))
+    print('Found {} running builds!'.format(num_builds['total_item_count']))
     for build in running_builds:
-        if build.slug != build_slug and build.triggered_workflow in workflow_names:
-            print("Build {} should be rolled, attempting abort".format(build.triggered_workflow))
+        if build['slug'] != build_slug and build['triggered_workflow'] in workflow_names:
+            print("Build {} should be rolled, attempting abort".format(build['triggered_workflow']))
             abort(build)
         else:
-            print("Build {} not eligible to be rolled, skipping".format(build.triggered_workflow))
+            print("Build {} not eligible to be rolled, skipping".format(build['triggered_workflow']))
     rolled_build_slugs = '\n'.join(rolled_build_slugs_list)
     return rolled_build_slugs
 
